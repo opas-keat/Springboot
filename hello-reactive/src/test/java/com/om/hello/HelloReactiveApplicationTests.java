a4ca27ef8308c33342b3fb90774de60046045de0
opas-keat <opas.keat@gmail.com>
2020-11-16 09:44:37 +0700

    first commit

---
+++ hello-reactive/src/test/java/com/om/hello/HelloReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.om.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
