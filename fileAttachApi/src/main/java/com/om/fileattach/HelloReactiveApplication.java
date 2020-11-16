ffc19286eeb5b05dbeb7ca062289a12c310d5e0d
opas-keat <opas.keat@gmail.com>
2020-11-16 10:37:15 +0700

    fileattach

---
+++ fileAttachApi/src/main/java/com/om/fileattach/HelloReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.om.fileattach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloReactiveApplication.class, args);
+	}
+
+}
